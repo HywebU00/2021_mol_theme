cb10f2c0ce5d103f2a1f6c07d6cb5bdc9e14cb9a
Miley <miley.ho@hyweb.com.tw>
2021-05-06 13:48:19 +0800

    first commit

---
+++ js/customize.js
@@ -0,0 +1,237 @@
+// 自行加入的JS請寫在這裡
+$(function() {
+    // 首頁輪播
+    $('.mpSlider').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        lazyLoaded: true,
+        lazyLoad: 'ondemand',
+        ease: 'ease',
+        asNavFor: '.slider-nav'
+    });
+    $('.slider-nav').slick({
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        asNavFor: '.mpSlider',
+        dots: false,
+        centerMode: true,
+        focusOnSelect: true
+    });
+    // 廣告輪播
+    $('.adSlider').slick({
+        mobileFirst: true,
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        autoplay: true,
+        arrow: true,
+        lazyLoaded: true,
+        lazyLoad: 'ondemand',
+        ease: 'ease',
+        responsive: [{
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 5,
+                slidesToScroll: 1,
+                arrows: true
+            }
+        }, {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                arrows: true
+            }
+        }, {
+            breakpoint: 575,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                arrows: true
+            }
+        }]
+    });
+    //燈箱slick+lightBox組合
+    $('.cp_slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 1500,
+        pauseOnHover: true,
+        pauseOnFocus: true,
+        focusOnSelect: true,
+        accessibility: true,
+        lazyLoad: 'ondemand',
+        ease: 'ease',
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                infinite: true,
+                dots: true
+            }
+        }, {
+            breakpoint: 545,
+            settings: {
+                arrows: true,
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        }, {
+            breakpoint: 480,
+            settings: {
+                arrows: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false
+            }
+        }]
+    });
+    $('.cp_slider').slickLightbox({
+        caption: 'caption',
+        lazyLoad: 'ondemand',
+        useHistoryApi: 'true',
+        ease: 'ease',
+        lazy: true
+    });
+    // 
+    $('.cppic_slider').slick({
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: false,
+        autoplaySpeed: 1500,
+        // pauseOnHover: true,
+        // pauseOnFocus: true,
+        // focusOnSelect: true,
+        // accessibility: true,
+        // lazyLoad: 'ondemand',
+        // ease: 'ease',
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                infinite: true,
+                dots: true
+            }
+        }, {
+            breakpoint: 545,
+            settings: {
+                arrows: true,
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        }, {
+            breakpoint: 480,
+            settings: {
+                arrows: true,
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                arrows: false
+            }
+        }]
+    });
+
+    // cp_photo
+    $('.Slider-for').on('init reInit afterChange', function(event, slick, currentSlide) {
+        var i = (currentSlide ? currentSlide : 0) + 1;
+        $('.controls').html(i + '/' + slick.slideCount);
+    });
+    $('.Slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        fade: true,
+        swipe: false,
+        swipeToSlide: false,
+        lazyLoad: 'ondemand',
+        asNavFor: '.Slider-nav',
+        infinite: true
+    });
+    $('.Slider-nav').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        asNavFor: '.Slider-for',
+        dots: true,
+        arrows: false,
+        lazyLoad: 'ondemand',
+        focusOnSelect: true,
+        infinite: true
+    });
+
+});
+$('.service ul').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 6,
+    pauseOnHover: true,
+    slidesToScroll: 1,
+    autoplay: false,
+    arrow: true,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 4,
+            slidesToScroll: 4,
+            infinite: true,
+            dots: false,
+            arrows: true
+        }
+    }, {
+        breakpoint: 992,
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            arrows: true
+        }
+    }, {
+        breakpoint: 575,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            arrows: true
+        }
+    }]
+});
+
+$(function() {
+    // search
+    var searchStatus = false; // 假的判斷式，先設定沒有開啟
+    $('.navigation').find('.searchbtn').click(function() {
+        $(this).siblings('.search').stop().slideToggle();
+        searchStatus = true;
+    })
+    if (searchStatus = true) {
+        $('body').keydown(function(e) {
+            if (e.keyCode == 27) {
+                $('.search_block .search').slideUp();
+            }
+        });
+    }
+
+    //顯示條件查詢開關
+    $('.btn-conditional').click(function(e) {
+        $(this).stop(true, true).toggleClass('open');
+        $(this).parent('.conditional_search').find('.flex-form').stop(true, true).slideToggle();
+        if ($(this).hasClass('open')) {
+            $('.btn-conditional').html('隱藏條件查詢');
+            $('.btn-conditional').attr('name', '隱藏條件查詢');
+        } else {
+            $('.btn-conditional').html("顯示條件查詢");
+            $('.btn-conditional').attr('name', '顯示條件查詢');
+        }
+    });    
+
+});
